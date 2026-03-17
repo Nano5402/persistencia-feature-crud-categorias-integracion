edcf88df43a61e07638cb645a15e4cd69ff7490a
Santiago C. <santiagocalvete69@gmail.com>
2026-03-16 20:49:07 -0500

    Se realiza subida con pequeño avance en la parte de categories, se procede a realizar testeo para verificar su respectivo funcionamiento.

---
+++ src/controllers/category.controller.js
@@ -0,0 +1,77 @@
+import { CategoryModel } from "../models/category.model.js";
+import { ProductModel } from "../models/product.model.js"; // Lo necesitaremos para la Fase 4
+
+export const getAllCategories = (req, res) => {
+  const categories = CategoryModel.findAll();
+  res.status(200).json({
+    success: true,
+    message: "Lista de categorías",
+    data: categories,
+    errors: [],
+  });
+};
+
+export const getCategoryById = (req, res) => {
+  const { id } = req.params;
+  const category = CategoryModel.findById(Number(id));
+  
+  if (!category) {
+    return res.status(404).json({
+      success: false,
+      message: `Categoría con ID ${id} no encontrada`,
+      data: [],
+      errors: [],
+    });
+  }
+  
+  res.status(200).json({
+    success: true,
+    message: "Categoría encontrada correctamente",
+    data: category,
+    errors: [],
+  });
+};
+
+export const createCategory = (req, res) => {
+  const { name } = req.body;
+  
+  if (!name) {
+    return res.status(400).json({
+      success: false,
+      message: "El nombre de la categoría es obligatorio",
+      data: [],
+      errors: [],
+    });
+  }
+
+  const newCategory = CategoryModel.create({ name });
+  res.status(201).json({
+    success: true,
+    message: "Categoría creada correctamente",
+    data: newCategory,
+    errors: [],
+  });
+};
+
+export const updateCategory = (req, res) => {
+  const { id } = req.params;
+  const updatedCategory = CategoryModel.update(Number(id), req.body);
+  
+  if (!updatedCategory) {
+    return res.status(404).json({
+      success: false,
+      message: `Categoría con ID ${id} no encontrada`,
+      data: [],
+      errors: [],
+    });
+  }
+  
+  res.status(200).json({
+    success: true,
+    message: "Categoría actualizada correctamente",
+    data: updatedCategory,
+    errors: [],
+  });
+};
+
+// El método deleteCategory lo completaremos en la Fase 4
